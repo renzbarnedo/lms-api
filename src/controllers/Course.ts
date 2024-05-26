0c72e0d3dd2180d78fbeb91bb5c4cdbb351aee46
Renz Aldwin Barnedo <2172078@example.com>
2024-05-26 23:24:46 +0800

    feat: add get and delete course controllers & routes

--- src/controllers/Course.ts
+++ src/controllers/Course.ts
@@ -23,4 +23,42 @@ const createCourse = (req: Request, res: Response, next: NextFunction) => {
     );
 };
 
-export default { createCourse };
+const getCourse = (req: Request, res: Response, next: NextFunction) => {
+  const technology = req.params.technology;
+
+  return Course.find({ technology })
+    .then((course) =>
+      course
+        ? res.status(200).json({ course })
+        : res.status(404).json({ message: "Not Found" })
+    )
+    .catch((error) =>
+      res.status(500).json({ createCourse: "createCourse", error })
+    );
+};
+
+const getAll = (req: Request, res: Response, next: NextFunction) => {
+  return Course.find()
+    .then((courses) => res.status(200).json({ courses }))
+    .catch((error) =>
+      res.status(500).json({ createCourse: "createCourse", error })
+    );
+};
+
+const deleteCourse = (req: Request, res: Response, next: NextFunction) => {
+  const { courseName } = req.params;
+
+  return Course.deleteOne({ name: courseName })
+    .then((result) =>
+      result.deletedCount
+        ? res
+            .status(200)
+            .json({ message: `Deleted ${courseName} successfully.` })
+        : res.status(404).json({ message: "Not Found" })
+    )
+    .catch((error) =>
+      res.status(500).json({ createCourse: "createCourse", error })
+    );
+};
+
+export default { createCourse, getCourse, getAll, deleteCourse };
--- src/routes/course.ts
+++ src/routes/course.ts
@@ -3,6 +3,23 @@ import courseController from "../controllers/Course";
 
 const router = Router();
 
-router.post("/course", courseController.createCourse);
+router.post("/courses/add", courseController.createCourse);
+router.get("/course", courseController.createCourse);
+router.get("/courses/info/:technology", courseController.getCourse);
+router.get("/courses/getall", courseController.getAll);
+router.delete("/courses/delete/:courseName", courseController.deleteCourse);
 
 export default router;
+
+/**
+ * 
+ * OK
+POST /api/v1.0/lms/courses/add/<coursename> Add new course to the system
+GET /api/v1.0/lms/courses/info/<technology> Fetches the course Details based on given technology
+GET /api/v1.0/lms/courses/getall Fetches all the course Details
+DELETE /api/v1.0/lms/courses/delete/<coursename> Deletes a course
+ 
+TODO:
+POST /api/v1.0/lms/company/register Register a new company
+GET /api/v/1.0/lms/courses/get/<technology>/ <durationFromRange>/<durationToRange> Fetches courses based on the given duration
+ */
